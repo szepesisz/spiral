84fcc10205ca6b7cec2ed4caa41e79bdf9457011
Szabolcs Szepesi <szepesi.szabolcs@rubin.hu>
2024-06-22 23:36:46 +0200

    up

--- spiral.py
+++ spiral.py
@@ -1,14 +1,11 @@
 
 def dirs():
-    d = 1j
     i = 0
     while i := i + 1:
-        d *= -1j
-        yield from ((int(d.imag), int(d.real)), ) * (i//2)
+        yield from ((lambda d: (int(d.imag), int(d.real)))(1j**i), ) * (i//2)
 
 
-def main():
-    a = 5
+def spiral(a):
     m = [[0 for _ in range(a)] for _ in range(a)]
     x = y = a//2
     for n, (dx, dy) in zip(range(num := a**2), dirs()):
@@ -17,6 +14,11 @@ def main():
 
     w = len(str(num))
     print(*(' '.join((str(n).ljust(w) for n in r)) for r in m), sep='\n')
+
+
+def main():
+    spiral(5)
+    spiral(4)
 
 
 if __name__ == '__main__':
