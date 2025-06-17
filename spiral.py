4467cfc630e893f4cdadba69528707a1ce15d8ce
szepesisz <szabolcs.szepesi@rubin.hu>
2025-06-17 12:25:46 +0200

    up

--- spiral.py
+++ spiral.py
@@ -3,20 +3,18 @@ def c_tup(z):
     return int(z.imag), int(z.real)
 
 
-def dirs():
-    i = 0
-    while i := i+1:
-        yield from i//2 * (c_tup(1j**i), )
+def dirs(a):
+    yield from sum(((i//2 * (c_tup(1j**i), )) for i in range(1, 2*a+1)), start=())
 
 
 def spiral(a):
     m = [[0 for _ in range(a)] for _ in range(a)]
     x = y = a//2
-    for n, (dx, dy) in zip(range(num := a**2), dirs()):
+    for n, (dx, dy) in enumerate(dirs(a)):
         m[x][y] = n
         x, y = x+dx, y+dy
 
-    w = len(str(num))
+    w = len(str(a**2))
     print(*(' '.join(str(n).ljust(w) for n in r) for r in m), sep='\n')
 
 
